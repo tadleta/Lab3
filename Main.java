58da3d26e13894dc3756f8e7cd423e67f1ff41d1
Tyler Adleta <tadleta.programming@gmail.com>
2020-09-15 21:34:21 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    int num;
+    
+    System.out.println("Enter a number to count down or up from:");
+    num = s.nextInt();
+    
+    if(num >= 0)
+      while(num > 0)
+      {
+        System.out.println(num + "...");
+        num--;
+      }
+    else if(num < 0)
+      while(num < 0)
+      {
+        System.out.println(num + "...");
+        num++;
+      }
+    System.out.println("Blast off!!!");
+  }
+}
